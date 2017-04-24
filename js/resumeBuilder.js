0a5f64a90d21ee15748891882f5b53629680a64d
Itrat Rahman <rahmanitrat@gmail.com>
2017-04-25 01:49:00 +0600

    Used the replace method to replace string portion

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,13 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 //$("#main").append("Itrat Rahman");
 
-var awesomeThoughts = "I am Itrat and I am AWESOME";
+var awesomeThoughts = "My name is Itrat and I am AWESOME";
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
 
 console.log(awesomeThoughts);
+
+console.log(funThoughts)
+
+//Appending fun thought to the page
+$("#main").append(funThoughts)
