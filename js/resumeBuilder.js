1188a84d394a6e8f418a94f4419d80f81c01140b
Itrat Rahman <rahmanitrat@gmail.com>
2017-04-25 13:08:53 +0600

    Created formatted variables and append it to the header div

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,13 +3,24 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 //$("#main").append("Itrat Rahman");
 
-var awesomeThoughts = "My name is Itrat and I am AWESOME";
+// var awesomeThoughts = "My name is Itrat and I am AWESOME";
 
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
-console.log(awesomeThoughts);
+// console.log(awesomeThoughts);
 
-console.log(funThoughts)
+// console.log(funThoughts);
 
-//Appending fun thought to the page
-$("#main").append(funThoughts)
+// //Appending fun thought to the page
+// $("#main").append(funThoughts);
+
+var name = "Itrat Rahman"
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Software Engineer"
+
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").prepend(formattedRole)
+$("#header").prepend(formattedName)
