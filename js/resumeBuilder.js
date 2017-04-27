278a8829680afc7dc193c2498936831e551f1fbe
Itrat Rahman <rahmanitrat@gmail.com>
2017-04-27 16:58:09 +0600

    Added Work Experience section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -155,3 +155,59 @@ if(bio.skills.length > 0){
 
 //     console.log(i);
 // }
+
+work = {
+
+    "jobs" : [
+
+        {
+            "employer":"Southeast Bank Limited",
+            "title":"Software Engineering Intern",
+            "location":"Dhaka",
+            "dates": "05/05/2016-05/08/2016",
+            "description": "Developed Optical Character Recognition System (contains machine learning classification in design pipeline) to convert raw bank documents into text documents."
+        },
+        {
+            "employer": "Insight Robotics",
+            "title": "Software Engineering Intern",
+            "location": "Hong Kong",
+            "dates": "09/11/2015-08/01/2016",
+            "description": "Successfully developed fast and efficient Smoke Detection Algorithm that the company decided to use on their fire detection robots for long term"
+
+        },
+        {
+            "employer": "University College London",
+            "title": "Summer Research Intern",
+            "location": "London",
+            "dates": "1/07/2014-30/09/2014",
+            "description": "Successfully researched and tested a new fibre-dispersion tolerant multi-subcarrier modulation scheme"
+
+        }
+    ]
+
+};
+
+
+for (job in work.jobs){
+
+    //Create a new div for work experience
+    $("#workExperience").append(HTMLworkStart);
+
+    //Concat employer and title
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+    $(".work-entry:last").append(formattedDescription);
+
+}
