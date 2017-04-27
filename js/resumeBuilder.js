e51726c9a7952bb04e538b23067fe4c174222c3f
Itrat Rahman <rahmanitrat@gmail.com>
2017-04-27 20:49:12 +0600

    Added button to internatlize name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -228,3 +228,20 @@ $(document).click(function(loc){
 
 });
 
+//A function for internationilizing name
+function inName(name){
+
+    name = name.trim().split(" ");
+
+    console.log(name);
+
+    name[1] = name[1].toUpperCase();
+
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    return name[0]+" "+name[1]
+
+
+}
+
+$("#main").append(internationalizeButton);
